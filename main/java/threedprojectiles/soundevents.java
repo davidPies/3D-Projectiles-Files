3cf668a178f145f3051626c2415d20772506f5d5
davidPies <2014119@edtools.psd401.net>
2016-05-12 09:10:23 -0700

    Update soundevents.java

--- main/java/threedprojectiles/soundevents.java
+++ main/java/threedprojectiles/soundevents.java
@@ -1,30 +1,17 @@
 package threedprojectiles;
 
-import net.minecraft.init.Bootstrap;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class soundevents {
-
-	public static final SoundEvent q;
+	public static SoundEvent whish;
 	
-	private static SoundEvent getRegisteredSoundEvent(String id)
-    {
-        SoundEvent soundevent = (SoundEvent)SoundEvent.soundEventRegistry.getObject(new ResourceLocation(id));
-
-            return soundevent;
-        }
-
-    static
-    {
-        if (!Bootstrap.isRegistered())
-        {
-            throw new RuntimeException("Accessed Sounds before Bootstrap!");
-        }
-        else
-        {
-	q = getRegisteredSoundEvent("sounds");
-        }
-    }
+	public static void registerSounds() {
+		whish = registerSound("whish");
+	}
+	private static SoundEvent registerSound(String soundName) {
+		final ResourceLocation soundID = new ResourceLocation(threedprojectiles.modid, soundName);
+		return GameRegistry.register(new SoundEvent(soundID).setRegistryName(soundID));
+	}
 }
